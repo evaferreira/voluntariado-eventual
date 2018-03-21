4b1ae9dba0c4db41c2a44f88cb291eb826fecb84
Alejandro Abraham <alejandro.abraham@globallogic.com>
2018-03-21 06:22:05 +0000

    Implementa scaffolding de datatables

--- resources/views/backoffice/partials/body-scripts.blade.php
+++ resources/views/backoffice/partials/body-scripts.blade.php
@@ -5,6 +5,7 @@
 <!-- AdminLTE App -->
 <script src="{{asset('/bower_components/admin-lte/dist/js/adminlte.min.js')}}"></script>
 
+@stack('additional_scripts')
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
      user experience. -->
--- resources/views/backoffice/partials/css.blade.php
+++ resources/views/backoffice/partials/css.blade.php
@@ -12,3 +12,5 @@
 <!-- Google Font -->
 <link rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+@stack('additional_css')
